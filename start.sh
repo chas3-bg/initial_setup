35ca774e98efb366a4cd44bf6e13a72da66974ed
chas3-bg <kmssec@proton.me>
2024-12-22 19:19:19 +0200

    Add more stuff

--- start.sh
+++ start.sh
@@ -5,3 +5,9 @@ echo "Please setup proton as rclone remote. Press enter when done."
 ansible-playbook wireguard-setup.yaml
 echo "Testing podman"
 podman run hello-world
+echo "Copy SSH keys to ~"
+mkdir ~/ssh
+rclone copy proton:ssh ~/ssh
+echo "Copy keepass key to ~"
+mkdir keepass
+rclone copy proton:KUR.key
